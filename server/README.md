0eabba702b590e4e5b3e64b7c16137d6844fef25
Emmalu-ux <genhualu22@gmail.com>
2025-09-22 16:35:08 +0800

    feat: add backend framework

---
+++ server/README.md
@@ -0,0 +1,22 @@
+##### 安装 MongoDB
+
+1. `brew tap mongodb/brew`
+2. `brew install mongodb-community@8.2`
+
+![image-20250922160516396](https://raw.githubusercontent.com/EmmaLu-ux/imageUpload_typora/master/uPic/2025_09_22_16_05_17_1758528317_1758528317576_9SSsaa_image-20250922160516396.png)
+
+##### 运行 MongoDB
+
+1. `brew services start mongodb-community@8.2`
+
+
+
+##### 查看 MongoDB 服务
+
+1. `ps aux | grep mongod`
+
+
+
+##### 停止 MongoDB
+
+1. `brew services stop mongodb-community@8.2`
---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import mongoose from "mongoose"
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3001
+const databaseURL = process.env.DATABASE_URL
+
+app.use(cors({
+    origin: [process.env.ORIGIN], // 允许的来源，可以有多个
+    credentials: true, // 开启凭证支持
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+}))
+/**
+ * 加载 cookie-parser 中间件，把请求头里的 Cookie 字符串解析成对象挂到 req.cookies，方便读取或校验用户的 cookie。
+ */
+app.use(cookieParser())
+/**
+ * 启用内置 JSON 解析中间件，自动把 Content-Type: application/json 的请求体解析成对象挂到 req.body，便于后续处理提交的数据。
+ */
+app.use(express.json())
+
+const server = app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
+
+mongoose.connect(databaseURL).then(() => {
+    console.log(`DB connection successfully.`)
+}).catch(error => {
+    console.log(error.message)
+})
